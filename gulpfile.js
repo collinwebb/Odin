56811e4d178174fe6dfb49919b8fdeebcd5e80bb
Collin Webb <collindwebb@gmail.com>
2015-12-08 13:24:00 -0800

    adds paths to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
+const paths = {
+	src: {
+		scripts: 'source/javascripts/**/*.js'
+	},
+	dest: {
+		scripts: 'public/javascripts'
+	}
+};
+
 gulp.task('default', () => {
-	return gulp.src('source/javascripts/script.js')
+	return gulp.src(paths.src.scripts)
 		.pipe(babel({
 			presets: ['es2015']
 		}))
-		.pipe(gulp.dest('public/javascripts'));
+		.pipe(gulp.dest(paths.dest.scripts));
 });
