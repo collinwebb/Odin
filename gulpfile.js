5c633ca48645c2abf39ebc37973a2b939e6dff38
Collin Webb <collindwebb@gmail.com>
2015-12-08 13:44:21 -0800

    adds gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-
+const sourcemaps = require('gulp-sourcemaps');
 
 const paths = {
 	src: {
@@ -15,10 +15,12 @@ const paths = {
 
 gulp.task('default', () => {
 	return gulp.src(paths.src.scripts)
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(concat('script.js'))
-		.pipe(uglify())
+		.pipe(sourcemaps.init())
+			.pipe(babel({
+				presets: ['es2015']
+			}))
+			.pipe(concat('script.js'))
+			.pipe(uglify())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(paths.dest.scripts));
 });
