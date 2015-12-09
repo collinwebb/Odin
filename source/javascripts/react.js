005a9dd3adabfe5ab5bc30303b36ceae80027be6
Collin Webb <collindwebb@gmail.com>
2015-12-09 13:17:30 -0800

    adds react-dom

--- source/javascripts/react.js
+++ source/javascripts/react.js
@@ -1,1 +1,14 @@
-const React = require('react')
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+// var Test = React.createClass({
+//   render: function(){
+//     return (
+//       <div>
+//         Derp Test!
+//       </div>
+//     )
+//   }
+// });
+//
+// ReactDOM.render(<Test />, document.getElementById('app'));
