f89a82361ec0b41e9de9cdbcae40a43c8a15a475
Collin Webb <collindwebb@gmail.com>
2016-02-02 15:18:25 -0800

    fixes react error

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 const gulp = require('gulp');
 const babel = require('babelify');
-const concat = require('gulp-concat');
 const mocha = require('gulp-mocha');
 const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
-const webpack = require('gulp-webpack');
 const del = require('del');
 const shell = require('gulp-shell');
 const streamify = require('gulp-streamify');
@@ -20,16 +17,6 @@ const path = {
 gulp.task('clean', function() {
 	return del(['public/**']);
 });
-
-// gulp.task('build', function () {
-//   return gulp.src(path.source)
-//     .pipe(sourcemaps.init())
-//     .pipe(babel())
-//     .pipe(concat(path.minifiedOut))
-// 		.pipe(uglify())
-//     .pipe(sourcemaps.write("."))
-//     .pipe(gulp.dest(path.public));
-// });
 
 gulp.task('build', function(){
   browserify('source/javascripts/react.jsx')
@@ -47,7 +34,7 @@ gulp.task('test', function () {
 
 gulp.task('watch', function() {
 	gulp.watch('gulpfile.js', shell.task(['gulp']));
-  gulp.watch(path.source, ['build']);
+  gulp.watch(path.source, ['default']);
 	gulp.watch(path.public + '/**/*.js', ['test']);
 });
 
