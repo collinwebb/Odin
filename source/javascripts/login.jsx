b5f931e6a3ec01b574bcc8bad1333a2a84356ce8
Collin Webb <collindwebb@gmail.com>
2016-02-05 22:37:11 -0800

    installs passport and starts login script

---
+++ source/javascripts/login.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+const Relay = require('react-relay');
+const passport = require('passport');
+
+module.exports = React.createClass({
+  render: function(){
+    return (
+      <>
+    );
+  }
+});
--- source/javascripts/react.jsx
+++ source/javascripts/react.jsx
@@ -2,12 +2,14 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const Relay = require('react-relay');
 const Test = require('./test.jsx');
+const Login = require('./login.jsx');
 
 var Example = React.createClass({
   render: function(){
     return (
       <Test />
-    )
+      <Login />
+    );
   }
 });
 
